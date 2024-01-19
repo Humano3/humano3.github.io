b9ab856e8aee70d18272c655b30c0dfc77909f4f
Humano3 <155685318+Humano3@users.noreply.github.com>
2024-01-18 21:08:28 -0600

    login.js

--- login.js
+++ login.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
-  console.log('hello, wrold!')}
-));
+  console.log('hello, wrold!');
+  var imputemail = document.getElementById('email');
+  var imputpassword = document.getElementById('password');
+console.log(inputemail);
+console.log(inputpassword);
+});
