d7ba4e3fff42eed193fcf6d36dac94268008a3aa
Humano3 <155685318+Humano3@users.noreply.github.com>
2024-02-06 20:52:01 -0600

    Delete function validarPassword() {        directory

--- function validarPassword() {       /validarcontra.js
+++
@@ -1,9 +0,0 @@
-function validarPassword() {
-      var password = document.getElementById("password").value;
-      if (password.length === 8) {
-        alert("Contraseña validadá y almacenada :3");
-      } 
-      else {
-        alert("||||Incorrect, debe de estasblecerse maximo con 8 caracteres||||");
-      }
-    }
